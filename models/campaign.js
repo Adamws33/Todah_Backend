70b3e47802189177fe49b8aee8d2bf467f099d40
Adam Smith <adamws33@gmail.com>
2018-03-12 17:13:28 -0400

    monday end code

--- models/campaign.js
+++ models/campaign.js
@@ -1,7 +1,10 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var campaign = sequelize.define('campaign', {
-  
+    orgname:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     chapname:{
       type: DataTypes.STRING,
     },
